7021fd852d0b52188b597810e6cffc2ccd08e14a
Hrusikesh <hrusikeshswain@safeguardglobal.com>
2022-10-26 17:38:01 +0530

    Starting a new project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.location.href = "myappscheme://";
+    // setTimeout(function () {
+    //     window.location.href = '';
+    // }, 1000);
+    // setTimeout(function () {
+    //   this.forceUpdate();
+      // window.open(
+      //   "safeguard://resetpassword?token=dfkjsdkfhjksdhfhjdshhjfgsdhjgfdhjgfjhs",
+      //   "_blank"
+      // );
+      // window.location = "safeguard://resetpassword?token=dfkjsdkfhjksdhfhjdshhjfgsdhjgfdhjgfjhs";
+    // }, 100);
+
+    // window.location = "safeguard://resetpassword?token=dfkjsdkfhjksdhfhjdshhjfgsdhjgfdhjgfjhs";
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
